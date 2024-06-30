649816ad28f60a1d325fc5c7bc9d1650a9aed3ae
Bobcoder0010 <dellizulter@gmail.com>
2024-06-29 21:38:10 -0500

    2nd day intern

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodypraser = require("body-parser");
 const bodyParser = require("body-parser");
 const app = express ();
 
-mongoose.connect('mongodb://localhost:2045/NODE',{
+mongoose.connect('mongodb://localhost:20455/NODE',{
 useNewUrlparse: true,
 useUnifiedTopology: true
 
@@ -20,6 +20,35 @@ app.get('/',async (req,res)=>{
     const items = await Item.find();
     res.render('index',{items:items    });
 });
+app.get('/create',(req,res)=>{
+    res.render('create');
+})
+
+app.post('/create',async(req,res)=>{
+    const newitems = new item({name :req.body.name})
+    await newitems.save();
+    res.redirect('/')
+})
+app.get('/update/:id',async (req,res)=>{
+    const item = await item.findbyid(req.params.id)
+    res.render('update',{ item:item});
+})
+
+app.get('/update/:id',async (req,res)=>{
+    await item.findbyidandupdate(req.params.id,{name:req.body.name})
+    res.redirect('/');
+
+});
+
+app.post('./delete/:id',async (req,res)=>{
+    await findbyidandremove(req.params.id);
+   res.redirect('/')
+ });
+
+
+
+
+
 
 
 
