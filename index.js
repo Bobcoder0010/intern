4d560ffe2e530f00b0ecc631568b18121bf7bafc
Bobcoder0010 <dellizulter@gmail.com>
2024-07-02 03:06:34 -0500

    third

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-const express = require("express")
+const express = require("express");
+const { create } = require("./controller/create");
 const app = express()
 app.get('/',(req,res)=>{
     res.send("hellow world") 
 })
+app.use(express.json());
+
+
+app.use("/",create)
+
+
+
+
+
+
+
 app.listen(5000,()=>{
     console.log ("server start vayoo")
 })
